d68bd2268d71be8e29dd915b3ffcb206361815d1
xhieu2206 <xhieu94@gmail.com>
2020-02-07 22:11:45 +0700

    Finish delete micropost w/o test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,17 +8,25 @@ class MicropostsController < ApplicationController
       flash[:success] = "Create micropost successfully"
       redirect_to root_url
     else
+      @feed_microposts = current_user.feed.paginate(page: params[:page], per_page: 10)
       render 'static_pages/home'
     end
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_path if @micropost.nil?
     end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
-  def home
-    @micropost = current_user.microposts.build unless !logged_in?
-  end
+    def home
+      @micropost = current_user.microposts.build unless !logged_in?
+      @feed_microposts = current_user.feed.paginate(page: params[:page], per_page: 10)
+    end
 
   def help
   end
